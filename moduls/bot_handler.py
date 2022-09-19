dedad7f0741f46fa0bbe4b5834fedfcdff8c4e55
Sa1avatus <110773221+Sa1avatus@users.noreply.github.com>
2022-09-19 08:44:22 +0300

    Update bot_handler.py

--- moduls/bot_handler.py
+++ moduls/bot_handler.py
@@ -3,6 +3,7 @@ from moduls import extensions as e
 import telebot
 from telegram_bot_pagination import InlineKeyboardPaginator
 from telebot import types as t
+import json
 
 bot = telebot.TeleBot(s.BOT_TOKEN)
 # Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
@@ -26,31 +27,32 @@ def handle_docs_audio(message: telebot.types.Message):
 
 @bot.message_handler(commands=['convert', 'values'])
 def convert(message: telebot.types.Message):
-    #data = []
+    data = s.EXCHANGE_DATA.copy()
     text = 'Выберите валюту из которой конвертировать:'
-    bot.send_message(message.chat.id, text, reply_markup=get_markup(mode='base'))
+    bot.send_message(message.chat.id, text, reply_markup=get_markup(mode='base', data=data))
 
 
 
 def base_handler(call):
-    #print(f'def base_handler(message: telebot.types.Message) {call.data.split("#")[1].split()}')
-    base = call.data.split('#')[1]
+    print(f'def base_handler(message: telebot.types.Message) {call.data}')
+    print(type(call.data.split('#')[1]))
+    data = json.loads((call.data.split('#')[1]).replace("'", "\""))
     text = 'Выберите валюту в которую конвертировать:'
-    bot.send_message(call.message.chat.id, text, reply_markup=get_markup(mode='quote', data=base), parse_mode='Markdown')
+    bot.send_message(call.message.chat.id, text, reply_markup=get_markup(data, mode='quote'), parse_mode='Markdown')
 
 
 def quote_handler(call):
-    #print(f'def quote_handler(message: telebot.types.Message, base): {call.data.split("#")[1].split()}')
-    quote, base = call.data.split('#')[1].split()
-    data = f'{base} {quote}'
+    print(f'def quote_handler(message: telebot.types.Message, base): {call.data}')
+    data = json.loads((call.data.split('#')[1]).replace("'", "\""))
     text = 'Введите количество валюты для конвертации:'
-    bot.send_message(call.message.chat.id, text, reply_markup=get_amount_markup(mode='amount', data=data), parse_mode='Markdown')
+    bot.send_message(call.message.chat.id, text, reply_markup=get_amount_markup(data, mode='amount'), parse_mode='Markdown')
 
 
 def amount_handler(call):
-    #print(f'def amount_handler(message: telebot.types.Message, base, quote): {call.data.split("#")[1].split()}')
-    amount, base, quote = call.data.split('#')[1].split()
-    convert_handler(call.message, base, quote, amount)
+    print(f'def amount_handler(message: telebot.types.Message, base, quote): {call.data}')
+    data = json.loads((call.data.split('#')[1]).replace("'", "\""))
+    #amount, base, quote = call.data.split('#')[1].split()
+    convert_handler(call.message, **data)
 
 
 def convert_handler(message: telebot.types.Message, base, quote, amount):
@@ -62,38 +64,51 @@ def convert_handler(message: telebot.types.Message, base, quote, amount):
         bot.reply_to(message, f'На текущий момент {amount} {base} ({e.CURRENCIES[base]}) можно обменять на {text} {quote} ({e.CURRENCIES[quote]})')
 
 
-# def get_markup(page=1, mode = '', data = ''):
+# def get_markup(data, page=1, mode = ''):
 #     print(f'def get_markup: {mode} {data}')
+#     data = json.loads((call.data.split('#')[-1]).replace("'", "\""))
 #     paginator = InlineKeyboardPaginator(
 #         len(e.CURRENCIES) // 10 + 1,
 #         current_page=page,
-#         data_pattern=f'page#{{page}} {data}'
+#         data_pattern=f'page#{{page}}#{data}'
 #     )
 #     curr_items = list(map(list, e.CURRENCIES.items()))[(page-1)*10:(page-1)*10 + 10]
 #
 #     for i in range(0, len(curr_items), 2):
-#         item1 = t.InlineKeyboardButton(f'{curr_items[i][0]}:\t{curr_items[i][1]}', callback_data=f'{mode}#{curr_items[i][0]} {data}')
-#         item2 = t.InlineKeyboardButton(f'{curr_items[i+1][0]}:\t{curr_items[i+1][1]}', callback_data=f'{mode}#{curr_items[i+1][0]} {data}')
+#         dt1 = dict(data).copy()
+#         dt2 = dict(data).copy()
+#         dt1[mode] = curr_items[i][0]
+#         dt2[mode] = curr_items[i+1][0]
+#         item1 = t.InlineKeyboardButton(f'{curr_items[i][0]}:\t{curr_items[i][1]}', callback_data=f'{mode}#{dt1}')
+#         item2 = t.InlineKeyboardButton(f'{curr_items[i+1][0]}:\t{curr_items[i+1][1]}', callback_data=f'{mode}#{dt1}')
 #         paginator.add_before(item1, item2)
 #     return paginator.markup
 
 
-def get_markup(mode = '', data = ''):
-    #print(f'def get_markup: {mode} {data}')
+def get_markup(data, mode = ''):
+    print(f'def get_markup: {mode} {data}')
     keyboard = t.InlineKeyboardMarkup()
     curr_items = list(map(list, s.CURRENCIES.items()))
     for i in range(0, len(curr_items), 2):
-        item1 = t.InlineKeyboardButton(f'{curr_items[i][0]}:\t{curr_items[i][1]}', callback_data=f'{mode}#{curr_items[i][0]} {data}')
-        item2 = t.InlineKeyboardButton(f'{curr_items[i+1][0]}:\t{curr_items[i+1][1]}', callback_data=f'{mode}#{curr_items[i+1][0]} {data}')
+        dt1 = dict(data).copy()
+        dt2 = dict(data).copy()
+        dt1[mode] = curr_items[i][0]
+        dt2[mode] = curr_items[i+1][0]
+        item1 = t.InlineKeyboardButton(f'{curr_items[i][0]}:\t{curr_items[i][1]}', callback_data=f'{mode}#{dt1}')
+        item2 = t.InlineKeyboardButton(f'{curr_items[i+1][0]}:\t{curr_items[i+1][1]}', callback_data=f'{mode}#{dt2}')
         keyboard.add(item1, item2)
     return keyboard
 
 
-def get_amount_markup(mode = '', data = ''):
+def get_amount_markup(data, mode = ''):
     keyboard = t.InlineKeyboardMarkup()
     for i in [10, 100, 1000]:
-        item1 = t.InlineKeyboardButton(f'{i}', callback_data=f'{mode}#{i} {data}')
-        item2 = t.InlineKeyboardButton(f'{i * 1000}', callback_data=f'{mode}#{i * 1000} {data}')
+        dt1 = data.copy()
+        dt2 = data.copy()
+        dt1['amount'] = i
+        dt2['amount'] = i * 1000
+        item1 = t.InlineKeyboardButton(f'{i}', callback_data=f'{mode}#{dt1}')
+        item2 = t.InlineKeyboardButton(f'{i * 1000}', callback_data=f'{mode}#{dt2}')
         keyboard.add(item1, item2)
     return keyboard
 
@@ -104,7 +119,7 @@ def start():
 
 @bot.callback_query_handler(func=lambda call: True)
 def tes(call):
-    #print(call.data)
+    print(f'def tes(call): {call.data}')
     mode = call.data.split('#')[0]
     if mode == 'base':
         base_handler(call)
