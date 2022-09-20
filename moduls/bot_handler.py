0c56e48b96fbb13c76757edb20193f574639baea
Sa1avatus <singilarity@gmail.com>
2022-09-20 09:27:34 +0300

    Final Version

--- moduls/bot_handler.py
+++ moduls/bot_handler.py
@@ -4,18 +4,17 @@ import telebot
 from telegram_bot_pagination import InlineKeyboardPaginator
 from telebot import types as t
 import json
+import ast
 
 bot = telebot.TeleBot(s.BOT_TOKEN)
 # Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message: telebot.types.Message):
-    text = f'Для начала работы введите команду боту \n' \
-           f'в формате:\n' \
-           f'<из какой валюты перевести> <в какую валюту перевести> <количество валюты>\n' \
-           f'Например: USD RUB 1000\n' \
-           f'Либо, наберите команду /values или /convert и выберите нужные валюты\n' \
-
-
+    text = f'''Для начала работы введите команду боту
+в формате:
+<из какой валюты перевести> <в какую валюту перевести> <количество валюты>
+Например: USD RUB 1000
+Либо, наберите команду /values или /convert и выберите нужные валюты'''
     bot.reply_to(message, text)
 
 
@@ -28,89 +27,85 @@ def handle_docs_audio(message: telebot.types.Message):
 @bot.message_handler(commands=['convert', 'values'])
 def convert(message: telebot.types.Message):
     data = s.EXCHANGE_DATA.copy()
+    data['m'] = 'b'
     text = 'Выберите валюту из которой конвертировать:'
-    bot.send_message(message.chat.id, text, reply_markup=get_markup(mode='base', data=data))
+    bot.send_message(message.chat.id, text, reply_markup=get_markup(data))
 
 
 
 def base_handler(call):
-    print(f'def base_handler(message: telebot.types.Message) {call.data}')
-    print(type(call.data.split('#')[1]))
-    data = json.loads((call.data.split('#')[1]).replace("'", "\""))
+    #print(f'def base_handler(message: telebot.types.Message) {call.data}')
+    #print(type(call.data.split('#')[1]))
+    data = data_normalisation(call.data.split('#')[-1])
+    data['m'] = 'q'
     text = 'Выберите валюту в которую конвертировать:'
-    bot.send_message(call.message.chat.id, text, reply_markup=get_markup(data, mode='quote'), parse_mode='Markdown')
+    bot.send_message(call.message.chat.id, text, reply_markup=get_markup(data), parse_mode='Markdown')
 
 
 def quote_handler(call):
-    print(f'def quote_handler(message: telebot.types.Message, base): {call.data}')
-    data = json.loads((call.data.split('#')[1]).replace("'", "\""))
+    #print(f'def quote_handler(message: telebot.types.Message, base): {call.data}')
+    data = data_normalisation(call.data.split('#')[-1])
+    data['m'] = 'am'
     text = 'Введите количество валюты для конвертации:'
-    bot.send_message(call.message.chat.id, text, reply_markup=get_amount_markup(data, mode='amount'), parse_mode='Markdown')
+    bot.send_message(call.message.chat.id, text, reply_markup=get_amount_markup(data), parse_mode='Markdown')
 
 
 def amount_handler(call):
-    print(f'def amount_handler(message: telebot.types.Message, base, quote): {call.data}')
-    data = json.loads((call.data.split('#')[1]).replace("'", "\""))
-    #amount, base, quote = call.data.split('#')[1].split()
+    #print(f'def amount_handler(message: telebot.types.Message, base, quote): {call.data}')
+    data = data_normalisation(call.data.split('#')[-1])
     convert_handler(call.message, **data)
 
 
-def convert_handler(message: telebot.types.Message, base, quote, amount):
+def convert_handler(message: telebot.types.Message, m, b, q, am):
     try:
-        text = e.Cryptoconverter.get_price(base, quote, amount)
+        text = e.Cryptoconverter.get_price(b, q, am)
     except e.APIException as exc:
         bot.reply_to(message, f'{exc}')
     else:
-        bot.reply_to(message, f'На текущий момент {amount} {base} ({e.CURRENCIES[base]}) можно обменять на {text} {quote} ({e.CURRENCIES[quote]})')
+        bot.reply_to(message, f'На текущий момент {am} {b} ({e.CURRENCIES[b]}) можно обменять на {text} {q} ({e.CURRENCIES[q]})')
 
 
-# def get_markup(data, page=1, mode = ''):
-#     print(f'def get_markup: {mode} {data}')
-#     data = json.loads((call.data.split('#')[-1]).replace("'", "\""))
-#     paginator = InlineKeyboardPaginator(
-#         len(e.CURRENCIES) // 10 + 1,
-#         current_page=page,
-#         data_pattern=f'page#{{page}}#{data}'
-#     )
-#     curr_items = list(map(list, e.CURRENCIES.items()))[(page-1)*10:(page-1)*10 + 10]
-#
-#     for i in range(0, len(curr_items), 2):
-#         dt1 = dict(data).copy()
-#         dt2 = dict(data).copy()
-#         dt1[mode] = curr_items[i][0]
-#         dt2[mode] = curr_items[i+1][0]
-#         item1 = t.InlineKeyboardButton(f'{curr_items[i][0]}:\t{curr_items[i][1]}', callback_data=f'{mode}#{dt1}')
-#         item2 = t.InlineKeyboardButton(f'{curr_items[i+1][0]}:\t{curr_items[i+1][1]}', callback_data=f'{mode}#{dt1}')
-#         paginator.add_before(item1, item2)
-#     return paginator.markup
+def get_markup(data, page=1):
+    #print(f'def get_markup: {data} {type(data)}')
+    data = data_normalisation(data)
+    mode = data['m']
+    paginator = InlineKeyboardPaginator(
+        len(e.CURRENCIES) // 10 + 1,
+        current_page=page,
+        data_pattern=f'page#{{page}}#{str(data).replace("{", "(").replace("}", ")")}'
+    )
+    curr_items = list(map(list, e.CURRENCIES.items()))[(page-1)*10:(page-1)*10 + 10]
 
-
-def get_markup(data, mode = ''):
-    print(f'def get_markup: {mode} {data}')
-    keyboard = t.InlineKeyboardMarkup()
-    curr_items = list(map(list, s.CURRENCIES.items()))
     for i in range(0, len(curr_items), 2):
-        dt1 = dict(data).copy()
-        dt2 = dict(data).copy()
+        dt1 = data.copy()
+        dt2 = data.copy()
         dt1[mode] = curr_items[i][0]
         dt2[mode] = curr_items[i+1][0]
         item1 = t.InlineKeyboardButton(f'{curr_items[i][0]}:\t{curr_items[i][1]}', callback_data=f'{mode}#{dt1}')
         item2 = t.InlineKeyboardButton(f'{curr_items[i+1][0]}:\t{curr_items[i+1][1]}', callback_data=f'{mode}#{dt2}')
-        keyboard.add(item1, item2)
-    return keyboard
+        paginator.add_before(item1, item2)
+    return paginator.markup
 
 
-def get_amount_markup(data, mode = ''):
+def get_amount_markup(data):
     keyboard = t.InlineKeyboardMarkup()
+    data = data_normalisation(data)
+    mode = data['m']
     for i in [10, 100, 1000]:
         dt1 = data.copy()
         dt2 = data.copy()
-        dt1['amount'] = i
-        dt2['amount'] = i * 1000
+        dt1['am'] = i
+        dt2['am'] = i * 1000
         item1 = t.InlineKeyboardButton(f'{i}', callback_data=f'{mode}#{dt1}')
         item2 = t.InlineKeyboardButton(f'{i * 1000}', callback_data=f'{mode}#{dt2}')
         keyboard.add(item1, item2)
     return keyboard
+
+
+def data_normalisation(data):
+    data = str(data).replace("(", "{").replace(")", "}")
+    data = ast.literal_eval(data)
+    return data
 
 
 def start():
@@ -119,16 +114,11 @@ def start():
 
 @bot.callback_query_handler(func=lambda call: True)
 def tes(call):
-    print(f'def tes(call): {call.data}')
-    mode = call.data.split('#')[0]
-    if mode == 'base':
-        base_handler(call)
-    elif mode == 'quote':
-        quote_handler(call)
-    elif mode == 'amount':
-        amount_handler(call)
-    elif mode == 'page':
-        page = int((call.data.split('#')[1]).split()[0])
+    #print(f'def tes(call): {call.data}')
+    data = data_normalisation(call.data.split('#')[-1])
+    mode = data['m']
+    if call.data.split('#')[0] == 'page':
+        page = int(call.data.split('#')[1])
         bot.delete_message(
             call.message.chat.id,
             call.message.message_id
@@ -136,11 +126,30 @@ def tes(call):
         bot.send_message(
             call.message.chat.id,
             'Выберите валюту:',
-            reply_markup=get_markup(page, mode=mode, data=call.data.split()[-1]),
+            reply_markup=get_markup(call.data.split('#')[-1], page),
+            parse_mode='Markdown'
+        )
+    elif mode == 'b':
+        base_handler(call)
+    elif mode == 'q':
+        quote_handler(call)
+    elif mode == 'am':
+        amount_handler(call)
+    elif mode == 'page':
+        page = int(call.data.split('#')[1])
+        bot.delete_message(
+            call.message.chat.id,
+            call.message.message_id
+        )
+        bot.send_message(
+            call.message.chat.id,
+            'Выберите валюту:',
+            reply_markup=get_markup(call.data.split('#')[-1], page),
             parse_mode='Markdown'
         )
     else:
-        print(call.data)
+        pass
+        #print(call.data)
 
 
 @bot.message_handler(content_types=['text'])
@@ -154,3 +163,5 @@ def convert_text(message: telebot.types.Message):
         bot.reply_to(message, f'{exc}')
     else:
         bot.reply_to(message, f'На текущий момент {amount} {base} ({e.CURRENCIES[base]}) можно обменять на {text} {quote} ({e.CURRENCIES[quote]})')
+
+
